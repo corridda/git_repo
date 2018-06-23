0604f34d58b065f7ec446e9200450c7a845565d6
corridda <corridda@yandex.ru>
2018-06-23 15:28:13 +0300

    ';' added

--- print_Hello.py
+++ print_Hello.py
@@ -1,7 +1,7 @@
 def main():
-    print("Hello, world!")
-    print("Hello, everybody!")
-    print("Hello!")
+    print("Hello, world!;")
+    print("Hello, everybody!;")
+    print("Hello!;")
 
 if __name__ == "__main__":
     main()
