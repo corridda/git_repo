fc247b527d886883ad4926fd0f27ed364af438b0
corridda <corridda@yandex.ru>
2018-06-23 15:50:53 +0300

    Merge branch 'master' into feature_hello_advanced
    # Conflicts:
    #	print_Hello.py

--- print_Hello.py
+++ print_Hello.py
@@ -1,9 +1,9 @@
 def main():
-    print("Hello, world!")
-    print("Hello, everybody!")
-    print("Hello!")
-    print("Hello my dear friend!")
-    print("Glad to see you again")
+    print("Hello, world!;")
+    print("Hello, everybody!;")
+    print("Hello!;")
+    print("Hello my dear friend!;")
+    print("Glad to see you again;")
 
 if __name__ == "__main__":
     main()
