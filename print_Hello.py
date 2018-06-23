9f024abc914a14412f3ae2f96d5d747e006aec9c
corridda <corridda@yandex.ru>
2018-06-23 16:54:44 +0300

    print("Cherry-pick_1_2;")

--- print_Hello.py
+++ print_Hello.py
@@ -5,6 +5,7 @@ def main():
     print("Hello my dear friend!;")
     print("Glad to see you again;")
     print("Cherry-pick_1_1;")
+    print("Cherry-pick_1_2;")
 
 if __name__ == "__main__":
     main()
